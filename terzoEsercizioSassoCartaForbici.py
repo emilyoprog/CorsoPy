db98b5f916e8ce2f8ec7265d6b8ed3ad6e62679a
Th3LastUch1ha <99171955+Th3LastUch1ha@users.noreply.github.com>
2023-01-23 02:53:42 +0100

    Update terzoEsercizioSassoCartaForbici.py

--- terzoEsercizioSassoCartaForbici.py
+++ terzoEsercizioSassoCartaForbici.py
@@ -1,21 +1,28 @@
+class c:
+    OK = '\033[92m'  # GREEN
+    WARNING = '\033[93m'  # YELLOW
+    FAIL = '\033[91m'  # RED
+    RESET = '\033[0m'  # RESET COLOR
+
 import random  # importo random per generare le scelte della CPU
 
-def checkWhoWin(player, computer):  #funzione che ritorna True se vince il giocatore o False se vince la CPU
+
+def checkWhoWin(player, computer):  # funzione che ritorna True se vince il giocatore o False se vince la CPU
     if player == computer:
-        print("Pareggio!")
+        print(c.WARNING + "Pareggio!" + c.RESET)
         return -1
     elif (player == "sasso" and computer == "carta") or (player == "carta" and computer == "forbici") or (
             player == "forbici" and computer == "sasso"):
-        print("Vince la CPU!")
+        print(c.FAIL + "Vince la CPU!" + c.RESET)
         return False
     else:
-        print("Vince il player!")
+        print(c.OK + "Vince il player!" + c.RESET)
         return True
 
 
 # dichiarazioni liste e variabili
 choices = ["sasso", "carta", "forbici"]
-gioco = ["si","no"]
+gioco = ["si", "no"]
 player = None
 countPlayer = 0
 countCPU = 0
@@ -30,9 +37,12 @@ while continueGame is True:
     while countPlayer != 3 and countCPU != 3:  # condizione che se verificata permette lo svolgimento di un nuovo round
         computer = random.choice(choices)
         while player not in choices:  # controllo se la scelta dell'utente sia una tra sasso, carta o forbice
-            player = input("\nSasso carta o forbici?: ").lower()
-        print(f"\n\nScelta CPU : {computer}\nScelta Player : {player}")  # stampo la scelta della CPU
-        flag = checkWhoWin(player, computer)  # richiamo la funzione checkWhoWin per controllare la possibile vittoria o pareggio dei due giocatori
+            player = input(c.OK + "\nSasso carta o forbici?: " + c.RESET).lower()
+            if (player not in choices):
+                print(c.FAIL + "\nInserire parametri accettati" + c.RESET)
+        print(
+            c.WARNING + f"\n\nScelta CPU : {computer}\nScelta Player : {player}" + c.RESET)  # stampo la scelta della CPU
+        flag = checkWhoWin(player,computer)  # richiamo la funzione checkWhoWin per controllare la possibile vittoria o pareggio dei due giocatori
         if flag is True:
             countPlayer += 1  # incremento il punteggio del giocatore in caso di sua vittoria
         elif flag is False:
@@ -40,20 +50,21 @@ while continueGame is True:
         elif flag == -1:
             pass  # in caso di pareggio non incremento alcun punteggio
 
-        print(f"\nPunteggio:\nPlayer: {countPlayer} \nCPU: {countCPU}")  # stampo i punteggi aggiornati dei due giocatori round per round
+        print(
+            c.WARNING + f"\nPunteggio:\nPlayer: {countPlayer} \nCPU: {countCPU}" + c.RESET)  # stampo i punteggi aggiornati dei due giocatori round per round
         player = None
 
     # essendo la vittoria assegnata a 3 vittorie di un giocatore controllo se uno dei due giocatori abbia vinto
     if countCPU == 3:
-        print("\nVince la CPU! Ritenta")
+        print(c.FAIL + "\nVince la CPU! Ritenta" + c.RESET)
     else:
-        print("\nVince il Player! Complimenti")
+        print(c.OK + "\nVince il Player! Complimenti" + c.RESET)
 
     while scelta not in gioco:  # chiedo fin quando l'input non sia corretto se l'utente vuole continuare a giocare creando una nuova partita
-        scelta = input("\nVuoi giocare ancora? ").lower()
+        scelta = input(c.OK + "\nVuoi giocare ancora? " + c.RESET).lower()
     if scelta == "si":
         continueGame = True
     else:
         continueGame = False
-        print("\nArrivederci!")
+        print("\nArrivederci!" + c.RESET)
     scelta = None
